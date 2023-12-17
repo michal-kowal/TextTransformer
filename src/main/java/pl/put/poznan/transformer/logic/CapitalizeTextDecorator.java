2c47048410ae06e9ed04594502f3cc7b1a180e1f
Marcin Łukomski <marcin.lukom@gmail.com>
2023-12-17 14:29:40 +0100

    Documentation for a single class
    Added JavaDoc comments to CapitalizeTextDecorator class

--- src/main/java/pl/put/poznan/transformer/logic/CapitalizeTextDecorator.java
+++ src/main/java/pl/put/poznan/transformer/logic/CapitalizeTextDecorator.java
@@ -4,22 +4,47 @@ import org.apache.commons.text.WordUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class CapitalizeTextDecorator extends TextDecorator{
+/**
+ * A decorator class that extends the TextDecorator class.
+ * It provides a functionality to capitalize text.
+ * @author Kacper Majorkowski
+ * @version 1.0
+ */
+public class CapitalizeTextDecorator extends TextDecorator {
+
     private static final Logger logger = LoggerFactory.getLogger(CapitalizeTextDecorator.class);
 
-    public CapitalizeTextDecorator(Transformer transformer){
+    /**
+     * Constructor for the CapitalizeTextDecorator class.
+     *
+     * @param transformer Object implementing Transformer interface.
+     */
+    public CapitalizeTextDecorator(Transformer transformer) {
         super(transformer);
     }
 
+    /**
+     * Transforms the input text by first applying the transformation of the base transformer
+     * and then capitalizing the result through the decorate method.
+     *
+     * @param text The input text to be transformed.
+     * @return The transformed and decorated text.
+     */
     @Override
-    public String transform(String text){
+    public String transform(String text) {
         String transformed = super.transform(text);
         String decorated = decorate(transformed);
         logger.debug(String.format("Transformed string %s to %s", text, decorated));
         return decorated;
     }
 
-    public String decorate(String text){
+    /**
+     * Capitalizes the input text using WordUtils.capitalize method.
+     *
+     * @param text The text to be capitalized.
+     * @return The capitalized text.
+     */
+    public String decorate(String text) {
         return WordUtils.capitalize(text);
     }
 }
